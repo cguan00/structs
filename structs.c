971402603f244b3f313c2a320e33b7effd4285cc
Christy Guan <cguan00@stuy.edu>
2019-10-16 13:18:54 -0400

    need to work on modify. added test cases

--- structs.c
+++ structs.c
@@ -3,6 +3,8 @@
 #include <time.h>
 
 struct student {char* name; int age;};
+
+
 //Write a function that returns an example of your struct when run.
 //This should not return the same values every time.
 struct student sample(){
@@ -12,21 +14,21 @@ struct student sample(){
   int x = rand();
   output.age = x;
 
-  output.name = *name;
+  output.name = "name";
   return output;
 }
 
 //Write a function that prints out variables of your structs type in a reasonable way.
 int print(struct student myStudent){
-  printf("name: %s", myStudent.name);
-  printf("age: %d", myStudent.age);
+  printf("name: %s\n", myStudent.name);
+  printf("age: %d\n", myStudent.age);
   return 0;
 }
 
 //Write a function that modifies values of your struct's type.
-int modify(struct student myStudent){
-  myStudent.name = "changed";
-  myStudent.age += 1;
+int modify(struct student myStudent, char* newName, int newAge){
+  myStudent.name = newName;
+  myStudent.age += newAge;
 }
 
 
@@ -35,6 +37,10 @@ int modify(struct student myStudent){
 int main(){
   struct student s1;
   s1 = sample();
+  print(s1);
+  modify(s1, "mine", 23);
+  printf("modified\n");
+  print(s1);
 
   return 0;
 }
