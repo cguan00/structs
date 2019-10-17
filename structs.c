b26428567a26ee08f0462de53c7097f76009b96b
Christy Guan <cguan00@stuy.edu>
2019-10-16 22:38:28 -0400

    fixed everything. should be done now

--- structs.c
+++ structs.c
@@ -1,43 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
+#include "structs.h"
 
-struct student {char* name; int age;};
+
 //Write a function that returns an example of your struct when run.
 //This should not return the same values every time.
-struct student* sample(){
-  srand(time(NULL));
-  struct student sample;
-  char* names[] = {"Joey", "Chandler", "Ross", "Monica", "Rachel", "Phoebe"};
-  int x = rand() % 20;
+struct student sample(){
+  int x = rand();
+  struct student output;
 
-  sample.age = x;
-  sample.name = names[x % 5];
+  char names[6][32] = {"Joey", "Chandler", "Ross", "Monica", "Rachel", "Phoebe"};
 
-  struct student* output = &sample;
+  strcpy(output.name, names[rand() % 6]);
+  output.age = rand() % 10 + 10;
+
   return output;
 }
 
 //Write a function that prints out variables of your structs type in a reasonable way.
-int printStudent(struct student *myStudent){
-  printf("name: %s\n", (*myStudent).name);
-  printf("age: %d\n", (*myStudent).age);
+int printStudent(struct student myStudent){
+  printf("name: %s\n", myStudent.name);
+  printf("age: %d\n", myStudent.age);
   return 0;
 }
 
 //Write a function that modifies values of your struct's type.
-int modify(struct student *myStudent, char *newName, int newAge){
-  if (newName != NULL){
-    myStudent->name = newName;
-  }
+int modify(struct student *myStudent, char* newName, int newAge){
+  strncpy(myStudent->name, newName, 32);
   myStudent->age = newAge;
-  return 0;
-}
-
-
-int main(){
-  printStudent(sample());
-
-
   return 0;
 }
