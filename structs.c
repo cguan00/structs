f91d7cd7f7b273198e5b5a51c3e47e83cf5880a3
Christy Guan <cguan00@stuy.edu>
2019-10-16 21:17:53 -0400

    fixed functions and added one test case

--- structs.c
+++ structs.c
@@ -5,36 +5,39 @@
 struct student {char* name; int age;};
 //Write a function that returns an example of your struct when run.
 //This should not return the same values every time.
-struct student sample(){
-  struct student output;
+struct student* sample(){
+  srand(time(NULL));
+  struct student sample;
+  char* names[] = {"Joey", "Chandler", "Ross", "Monica", "Rachel", "Phoebe"};
+  int x = rand() % 20;
 
-  srand( time(NULL) );
-  int x = rand();
-  output.age = x;
+  sample.age = x;
+  sample.name = names[x % 5];
 
-  output.name = *name;
+  struct student* output = &sample;
   return output;
 }
 
 //Write a function that prints out variables of your structs type in a reasonable way.
-int print(struct student myStudent){
-  printf("name: %s", myStudent.name);
-  printf("age: %d", myStudent.age);
+int printStudent(struct student *myStudent){
+  printf("name: %s\n", (*myStudent).name);
+  printf("age: %d\n", (*myStudent).age);
   return 0;
 }
 
 //Write a function that modifies values of your struct's type.
-int modify(struct student myStudent){
-  myStudent.name = "changed";
-  myStudent.age += 1;
+int modify(struct student *myStudent, char *newName, int newAge){
+  if (newName != NULL){
+    myStudent->name = newName;
+  }
+  myStudent->age = newAge;
+  return 0;
 }
 
 
-
-
 int main(){
-  struct student s1;
-  s1 = sample();
+  printStudent(sample());
+
 
   return 0;
 }
